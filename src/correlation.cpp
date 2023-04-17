b4566b0e732dfe58d459a323c98877bb836313fd
PETIT Thomas <thomas.petit@telecomnancy.eu>
2023-04-17 18:37:44 +0200

    [+] correlation complete

--- src/correlation.cpp
+++ src/correlation.cpp
@@ -6,17 +6,41 @@
 #include <math.h>
 using namespace std;
 
-double correlation(multimap<string, double> main_map, multimap<string, double> second_map, vector<double, double>)
+double correlation(multimap<string, double> main_map, multimap<string, double> second_map, vector<double, double> couple)
 {
-    return (esperance(main_map, second_map) - esperance(main_map) * esperance(second_map)) / (ecart_type(main_map) * ecart_type(second_map));
+    return (esperance(couple) - esperance(main_map) * esperance(second_map)) / (ecart_type(main_map) * ecart_type(second_map));
 }
 
 double esperance(multimap<string, double> map)
 {
+    multimap<double, int> probability_map;
+    for (multimap<string, double>::iterator it = map.begin(); it != map.end(); ++it)
+    {
+        probability_map.find(it->second)++;
+    }
+    int n = map.size();
+    double esperance = 0;
+    for (multimap<double, int>::iterator it = probability_map.begin(); it != probability_map.end(); ++it)
+    {
+        esperance += it->first * (it->second) / n;
+    }
+    return esperance;
 }
 
-double esperance(multimap<string, double> map1, multimap<string, double> map2)
+double esperance(vector<double, double> couple)
 {
+    multimap<double, int> probability_map;
+    for (vector<double, double>::iterator it = couple.begin(); it != couple.end(); ++it)
+    {
+        probability_map.find(it[0] * it[1])++;
+    }
+    int n = couple.size();
+    double esperance = 0;
+    for (multimap<double, int>::iterator it = probability_map.begin(); it != probability_map.end(); ++it)
+    {
+        esperance += it->first * (it->second) / n;
+    }
+    return esperance;
 }
 
 double ecart_type(multimap<string, double> map)
