99457efb2e18d1b43b3d0a6e6ac86373d8185729
NoirayMoussu Camille <camille.noiray-moussu@telecomnancy.eu>
2023-04-22 20:28:29 +0200

    [+] new main function

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include "newBayNodeBuilder.hpp"
+
+using namespace std;
+
+int main()
+{
+    vector<Attribute> list_att = setupNodesAndLinks();
+    Attribute first = list_att[0];
+    list_att = removeEl(list_att, first);
+    vector<Attribute> Suiv = pot(first, list_att);
+    vector<BayesianNode> S;
+    S.push_back(BayesianNode(&first));
+    vector<BayesianNode> S_prev = S;
+    while (list_att.size() != 0 && Suiv.size() != 0)
+    {
+        BayesianNode bayNode = BayesianNode(&Suiv[0]);
+        list_att = removeEl(list_att, Suiv[0]);
+        S_prev = S;
+        newBayNodeBuilder(bayNode, S_prev, list_att);
+        S.push_back(bayNode);
+
+        Suiv = removeEl(Suiv, Suiv[0]);
+    }
+}
